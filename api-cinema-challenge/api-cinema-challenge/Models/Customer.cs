c144009172072d67538b8eb6ceb02e63cdbff1b2
Snorre Aldstedt <snorre.alstedt@experisacademy.no>
2025-08-22 11:07:00 +0200

    Create models classes

---
+++ api-cinema-challenge/api-cinema-challenge/Models/Customer.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Mail;
+
+
+namespace api_cinema_challenge.Models
+{
+    [Table("Customers")]
+    public class Customer
+    {
+        [Key]
+        public int Id {  get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Column("phone")]
+        public string Phone { get; set; }
+
+        [Column("createdAt")]
+        public DateTime CreatedAt { get; set; }
+
+        [Column("updatedAt")]
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
---
+++ api-cinema-challenge/api-cinema-challenge/Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_cinema_challenge.Models
+{
+    [Table("Movies")]
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column("title")]
+        public string Title { get; set; }
+
+        [Column("rating")]
+        public string Rating { get; set; }
+
+        [Column("description")]
+        public string Description { get; set; }
+
+        [Column("runtimeMins")]
+        public int RuntimeMins { get; set; }
+
+        [Column("createdAt")]
+        public DateTime CreatedAt { get; set; }
+
+        [Column("updatedAt")]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
